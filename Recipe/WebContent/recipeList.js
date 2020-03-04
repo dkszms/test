e5f80f870654a450f1f7e243194dc6007fc992ce
User <User@DESKTOP-G82904M>
2020-03-04 18:37:41 +0900

    이벤트 추가 및 테스트

--- Recipe/WebContent/recipeList.js
+++ Recipe/WebContent/recipeList.js
@@ -1,18 +1,23 @@
-// window.onload = function() {
-	window.addEventListener("load",function(){
-			alert("test");
-			var recipeId = this.document.getElementsById("recipe-search");
-			recipeId.onclick = function() {
-			alert("으아아아");
-		}
-		// var recipeName = this.document.getElementsById("recipe-name");
-		// recipeName.onclick = function() {
-		// 	alert("으아아아");
-		// }
-		// var recipeDesc = this.document.getElementsById("recipe-description");
-		// recipeDesc.onclick = function() {
-		// 	alert("으아아아");
-		// }
-	});
+window.addEventListener("load",function(){
 	
-// } 
+	var recipeName = document.getElementById("recipe-name");
+	
+	var recipeId = document.getElementById("recipe-search");
+	recipeId.onclick = function() {
+		alert("으아아아 "+recipeName.value);
+		
+
+	}
+
+	var recipeTable = document.getElementById("recipe-table");
+	recipeTable.onclick = function() {
+		
+		//var tds = recipeTable.getElementsByTagName("td");
+		//var tds = recipeTable.getElementsByClassName("recipe-id");
+		var tds = recipeTable.getElementsByTagName("td");
+		
+		alert("test1212=  "+tds[0].value);
+	}
+});
+
+	
--- Recipe/WebContent/recipeList.jsp
+++ Recipe/WebContent/recipeList.jsp
@@ -19,22 +19,23 @@
 			border: 1px solid #444444;
 		}
 	</style>
-	<script src="recipeList.js"> alert("ddd");</script>
+	<script src="recipeList.js"></script>
 </head>
 <body>
 		레시피 목록
 	<input type="text" id="recipe-name"/>
 	<input type="button" value="검색" id="recipe-search"/>
 	<input type="button" value="등록" id="recipe-regist"/>
-	<table>
-		<c:forEach var="map" items="${al}" varStatus="status">
-			<tr id="recipe-detail">
+	<table id="recipe-table">
+		<c:forEach var="map" items="${al}" varStatus="status"> 
+			<tr>
 				<td id="recipe-id">${map.id}</td>
-				<td id="recipe-name">${map.name}</td>
-				<td id="recipe-description">${map.description}</td>
+				<td>${map.name}</td>
+				<td>${map.description}</td>
 			</tr>
 		</c:forEach>
 	</table>
-	<input type="button" value="클릭" id="tttt"/>
+	<input type="button" value="클릭" id="tttt" />
+	<button id="tt"">테스트 </button>
 </body>
 </html>
